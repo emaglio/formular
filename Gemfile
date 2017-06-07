acb282891268d364fffa3e6ce8601f9fc53f4319
emaglio <emanuele.magliozzi@gmail.com>
2017-06-07 17:03:12 +1000

    Basic Reform and Cell object implemented

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in formular.gemspec
 gemspec
+
+group :test do
+  gem 'reform', '>= 2.3.0.rc1'
+  gem 'reform-rails', '>= 0.2.0.rc1'
+end
--- test/doc/bootstrap3/test/new_test.rb
+++ test/doc/bootstrap3/test/new_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 require 'formular/helper'
 require 'trailblazer/cell'
 require 'cell/slim'
+require 'reform'
 
 class User::Cell
   class New < Trailblazer::Cell
     include Cell::Slim
-    include Formular::RailsHelper
     include Formular::Helper
 
     self.view_paths = ['test/doc/bootstrap3/concept']
@@ -22,12 +22,25 @@ class User::Cell
   end
 end
 
+
+module User::Contract
+  class New < Reform::Form
+    property :email
+    property :firstname
+    property :lastname
+    property :password, virtual: true
+    property :confirm_password, virtual: true
+
+  end
+end
+
 class TestNew < Minitest::Spec
   describe 'valid, initial rendering' do
-    let(:model) { User.new(1, "Luca", "Rossi", "luca@email.com", "password", "password", []) }
+    let(:model) { User.new(1, "Luca", "Rossi", "luca@email.com", "password", "password") }
 
     it do
-      User::Cell::New.new(model).().must_equal %(<div>New</div><form action="/posts" method="post" accept-charset="utf-8"><input name="utf8" type="hidden" value="✓"/><input name="comment[id]" id="comment_id" value="1" type="text"/><textarea name="comment[body]" id="comment_body">Nice!</textarea><input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/><input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/><input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/><input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/><input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/><input name="comment[uuid]" id="comment_uuid" value="0x" type="text"/><input value="Submit" type="submit"/></form>)
+      form = User::Contract::New.new(model)
+      User::Cell::New.new(form).().must_equal %(<div>New</div><form action="/posts" method="post" accept-charset="utf-8"><input name="utf8" type="hidden" value="✓"/><input name="comment[id]" id="comment_id" value="1" type="text"/><textarea name="comment[body]" id="comment_body">Nice!</textarea><input value="0" name="comment[public]" type="hidden"/><input value="true" name="comment[public]" id="comment_public" type="checkbox" checked="checked"/><input name="comment[replies][][content]" id="comment_replies_0_content" value="some exciting words" type="text"/><input name="comment[replies][][content]" id="comment_replies_1_content" type="text"/><input name="comment[owner][name]" id="comment_owner_name" value="Joe Blog" type="text"/><input name="comment[owner][email]" id="comment_owner_email" value="joe@somewhere.com" type="text"/><input name="comment[uuid]" id="comment_uuid" value="0x" type="text"/><input value="Submit" type="submit"/></form>)
     end
   end
 end
