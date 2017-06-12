a726b4a21c27a1567273d768ff5b0097046010a1
emaglio <emanuele.magliozzi@gmail.com>
2017-06-12 19:37:54 +1000

    Select input added

--- test/doc/bootstrap3/test/new_user_test.rb
+++ test/doc/bootstrap3/test/new_user_test.rb
@@ -27,6 +27,8 @@ module User::Contract
     property :email
     property :firstname
     property :lastname
+    property :gender
+    property :dob #TODO: it would be cool to have an icon of this input like a calendar for the datetime-picker
     property :password, virtual: true
     property :confirm_password, virtual: true
 
@@ -42,7 +44,7 @@ end
 
 # basic form
 class TestNewUser < Minitest::Spec
-  let(:model) { User.new(1, "Luca", "Rossi", "luca@email.com", "password", "password") }
+  let(:model) { User.new(1, "Luca", "Rossi", "Male", "01/01/1980", "luca@email.com", "password", "password") }
   let(:new_form) { <<-XML
     <form id="new_user" action="/users" method="post" accept-charset="utf-8">
       <input name="utf8" type="hidden" value="✓"/>
@@ -51,6 +53,16 @@ class TestNewUser < Minitest::Spec
       </div>
       <div class="form-group">
         <input placeholder="Lastname" name="lastname" id="lastname" value="Rossi" type="text" class="form-control"/>
+      </div>
+      <div class="form-group">
+        <select name="gender" id="gender" class="form-control">
+          <option value="Male" selected="selected">Male</option>
+          <option value="Female">Female</option>
+          <option value="You">You</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <input placeholder="Date of birth:" name="dob" id="dob" value="01/01/1980" type="text" class="form-control"/>
       </div>
       <div class="form-group">
         <input placeholder="Email" name="email" id="email" value="luca@email.com" type="text" class="form-control"/>
@@ -74,6 +86,16 @@ class TestNewUser < Minitest::Spec
       </div>
       <div class="form-group">
         <input placeholder="Lastname" name="lastname" id="lastname" value="Rossi" type="text" class="form-control"/>
+      </div>
+      <div class="form-group">
+        <select name="gender" id="gender" class="form-control">
+          <option value="Male" selected="selected">Male</option>
+          <option value="Female">Female</option>
+          <option value="You">You</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <input placeholder="Date of birth:" name="dob" id="dob" value="01/01/1980" type="text" class="form-control"/>
       </div>
       <div class="form-group has-error">
         <input placeholder="Email" name="email" id="email" value="" type="text" class="form-control"/>
@@ -134,6 +156,19 @@ class TestNewUser < Minitest::Spec
     XML
   }
 
+  let(:select_input) { <<-XML
+    <form id="new_user" action="/users" method="post" accept-charset="utf-8">
+      <div class="form-group">
+        <select name="gender" id="gender" class="form-control">
+          <option value="Male" selected="selected">Male</option>
+          <option value="Female">Female</option>
+          <option value="You">You</option>
+        </select>
+      </div>
+    </form>
+    XML
+  }
+
 
 
   it "valid inital rendering" do
@@ -143,6 +178,7 @@ class TestNewUser < Minitest::Spec
     assert_xml_contain html, input_no_error
     assert_not_xml_contain html, input_with_error
     assert_xml_contain html, input_password_type
+    assert_xml_contain html, select_input
     assert_xml_contain html, submit_button
     assert_xml_equal html, new_form
   end
